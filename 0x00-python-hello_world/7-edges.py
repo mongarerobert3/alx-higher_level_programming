10cbc6c8417e128f9b32f9f5e2f0676e551cf6e5
mongarerobert3 <mongarerobert3@gmail.com>
2022-05-30 20:05:16 +0300

    string formatting

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -5,4 +5,4 @@ word_last_2 = word
 middle_word = word
 print(f"First 3 letters:{word_first_3[:3]}")
 print(f"Last 2 letters: {word_last_2[-2:]}")
-print(f"Middle word: {middle_word[1:9]}")
+print(f"Middle word: {middle_word[1:-1]}")

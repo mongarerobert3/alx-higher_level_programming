ec879f0ffab3cb39b6190f4a6c5cdc2f2cb8fc43
mongarerobert3 <mongarerobert3@gmail.com>
2022-06-02 13:40:22 +0300

    prints a string using the sys module

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -2,4 +2,5 @@
 
 
 import sys
-sys.stdout.write("and that piece of art is useful - Dora Korpar, 2015-10-19", end=" ")
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)

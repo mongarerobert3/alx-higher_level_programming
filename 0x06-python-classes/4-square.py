75577b0798b3e059802b70b7bdf6fab9ce0e947f
mongarerobert3 <mongarerobert3@gmail.com>
2022-06-21 22:25:49 +0300

    Setter and getter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""2-square.py"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size=0):
+        """
+        Creates an instance of Square
+        Args:
+            size: size of the square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter and setter for private size variable"""
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """Finds the area of a square"""
+
+        return self.__size * self.__size

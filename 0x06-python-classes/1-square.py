eed1caec9c48d2b7c53c7a376985035fbd7b94de
mongarerobert3 <mongarerobert3@gmail.com>
2022-06-21 21:21:59 +0300

    creating a private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+    square
+"""
+
+
+class Square:
+    """defines a square"""
+    def __init__(self, size):
+        """Private instance attribute
+            Args:
+                size of the square
+        """
+        self.__size = size

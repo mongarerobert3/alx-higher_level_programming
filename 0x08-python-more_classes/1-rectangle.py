0c21a4c90ee37934a95977ccfba5be816fe29cd3
mongarerobert3 <mongarerobert3@gmail.com>
2022-06-27 17:19:04 +0300

    continuation of the rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-"""writing a rectangle"""
+"""Real definition of a rectangle"""
 
 
 class Rectangle:
-    """properties of the class"""
+    """Defines the implementation of a rectangle"""
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Property getter, for retreiving"""
+        """Property retriever, for retreiving"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Property setter"""
+        """Property setter, for setting"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -31,9 +32,8 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """Property setter"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value

e8eb983ed54d7199e84c4fe168bffed9a91bb859
mongarerobert3 <mongarerobert3@gmail.com>
2022-06-27 16:14:58 +0300

    0 & 1

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+"""writing a rectangle"""
+class Rectangle:
+    """empty class"""
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""writing a rectangle"""
+class Rectangle:
+    """properties of the class"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property 
+    def width(self):
+        """Property getter, for retreiving"""
+        return self.__width
+    
+    @width.setter
+    def width (self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Property retriever, for retreiving
+        the rectangle height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value

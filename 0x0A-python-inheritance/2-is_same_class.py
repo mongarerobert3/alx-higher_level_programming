cfd639bc49a09db2b41d278d530274aee60583db
mongarerobert3 <mongarerobert3@gmail.com>
2022-07-04 22:36:53 +0300

    Exact same object

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -8,6 +8,6 @@ def is_same_class(obj, a_class):
         obj - object of the class
         a_class - the class
     """
-    if isinstance(type(obj), a_class):
+    if (type(obj) == a_class):
         return True
     return False

4ca5af09c9258708faac87dd481f7524b8677bbd
mongarerobert3 <mongarerobert3@gmail.com>
2022-07-04 23:20:51 +0300

    Rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""7-base_geometry.py"""
+
+
+class BaseGeometry:
+    """Implements a basic geometric shape"""
+
+    def area(self):
+        """Raises Exception with message 'area() is not implemented'"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates the value of a given name
+        Args:
+            name: string
+            value: integer value of given name
+        Returns: None
+        """
+        # if value is not an integer
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        # if value is less or equal to 0
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+    def __init__(self, width, height):
+        """setting width and height"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height

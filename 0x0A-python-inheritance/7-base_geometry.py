753784adfb30d9df8f750b6d2471ede6119d0e17
mongarerobert3 <mongarerobert3@gmail.com>
2022-07-04 23:08:14 +0300

    Integer validator

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -16,7 +16,7 @@ class BaseGeometry:
             name(str) - input name as string
             value(int): pararams validator
         """
-        if self.value != type(int):
+        if value != type(int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))

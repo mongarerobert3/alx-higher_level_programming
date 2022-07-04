b3528964af594b704e0722fb9832f54b3ce58fa0
mongarerobert3 <mongarerobert3@gmail.com>
2022-07-04 23:00:54 +0300

    Improve Geometry

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -5,4 +5,7 @@
 class BaseGeometry():
     """Instance method area"""
     def area(self):
+        """Raises an exception because...
+        area is not implemented
+        """
         raise("area() is not implemented")

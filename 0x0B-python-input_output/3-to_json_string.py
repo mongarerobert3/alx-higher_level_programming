60fa29ffc1bbb80d71d4e9900ecfb8cb6dd424f3
mongarerobert3 <mongarerobert3@gmail.com>
2022-07-05 22:10:24 +0300

    To JSON string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""function that returns the JSON representation of an object (string):"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """logic"""
+    return json.dumps(my_obj)

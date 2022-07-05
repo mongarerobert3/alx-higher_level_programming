28db2e33cd5c3c6f9977c35e7058dcacea41fa17
mongarerobert3 <mongarerobert3@gmail.com>
2022-07-05 13:01:40 +0300

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,4 @@
 def read_file(filename=""):
     """opening the file"""
     with open(filename, encoding="utf_8") as f:
-        read_data = f.read()
-        print(read_data)
+        print(f.read(), end="")

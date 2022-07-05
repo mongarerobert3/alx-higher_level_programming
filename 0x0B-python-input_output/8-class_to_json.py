5b2f3305189105cf3a7b9b4f46452ea90af8831f
mongarerobert3 <mongarerobert3@gmail.com>
2022-07-05 22:57:44 +0300

    Class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""a function that returns the dictionary description with simple data structure 
-(list, dictionary, string, integer and boolean) 
+"""a function that returns the dictionary
+description with simple data structure
+(list, dictionary, string, integer and boolean)
 for JSON serialization of an object:"""
 
 

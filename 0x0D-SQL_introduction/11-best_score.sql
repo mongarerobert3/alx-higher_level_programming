db017220f520f744d5e8e60604af893046554a12
courte_labs <courte_labs@kali.courte_labs>
2022-08-09 16:08:27 +0300

    Select the best

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- lists all records with a score >= 10
-SELECT score, name FROM second_table WHERE score >= ORDER BY score DESC, name;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC, name;

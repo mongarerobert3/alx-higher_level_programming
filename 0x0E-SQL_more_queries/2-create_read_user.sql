9c97cc97eb224545b4ac6c04a3b1608e66754104
courte_labs <courte_labs@kali.courte_labs>
2022-08-11 01:12:15 +0300

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -10,4 +10,4 @@ CREATE USER IF NOT EXISTS user_0d_2@'localhost';
 SET PASSWORD FOR user_0d_2@localhost = 'user_0d_2_pwd';
 
 -- grant the permission
-GRANT SELECT ON hbtn_0d_2.* ON TO user_0d_2@localhost;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;

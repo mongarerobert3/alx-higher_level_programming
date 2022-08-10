bb2f48ef178de2cdd799424cb6411e792896df90
courte_labs <courte_labs@kali.courte_labs>
2022-08-11 01:10:25 +0300

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,7 +4,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- create the user
-CREATE USER IF NOT EXISTS user_0d_2@localhost;
+CREATE USER IF NOT EXISTS user_0d_2@'localhost';
 
 /* SET password for new user */
 SET PASSWORD FOR user_0d_2@localhost = 'user_0d_2_pwd';

8de1503338474823cf826bece902ac6c5ba82f0f
courte_labs <courte_labs@kali.courte_labs>
2022-08-11 01:06:13 +0300

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,10 +1,10 @@
 -- script that creates the database hbtn_0d_2 and the user user_0d_2
 
 -- craete the database
-CREATE DATABASE hbtn_0d_2;
+CREATE DATABASE IF EXISTS hbtn_0d_2;
 
 -- create the user
-CREATE USER user_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2;
 
 -- grant the permission
 GRANT SELECT ON hbtn_0d_2 ON TO user_0d_2@localhost;

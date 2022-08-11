c56cec588ecd6377bb90ba60c240cb965f863c35
courte_labs <courte_labs@kali.courte_labs>
2022-08-11 21:29:21 +0300

    Genre ID by show

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id FROM hbtn_0d_tvshows;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;

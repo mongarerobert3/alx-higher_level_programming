3bff41123fbb59d710a42f31c451b9b30b0d59e4
courte_labs <courte_labs@kali.courte_labs>
2022-08-11 21:31:13 +0300

    Genre ID by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,5 @@
--- script that lists all cities contained in the database hbtn_0d_usa.
--- Each record should display: cities.id - cities.name - states.name
--- Results must be sorted in ascending order by cities.id
-SELECT cities.id, cities.name, states.name FROM cities;
-FROM cities INNER JOIN states ON cities.state_id = states_id
-ORDER BY cities.id ASC;
+-- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id FROM hbtn_0d_tvshows;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;

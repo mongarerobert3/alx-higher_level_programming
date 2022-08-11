94cbbe7cd938c313cc6e478697e54c4371223b26
courte_labs <courte_labs@kali.courte_labs>
2022-08-11 21:05:04 +0300

    Cities of California

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,3 @@
 -- script that lists all the cities of California
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(id, name) VALUES(California);
-SELECT California FROM cities ORDER BY ASC;
+SELECT name, id FROM cities WHERE name = 'California'ORDER BY cities.id ASC;

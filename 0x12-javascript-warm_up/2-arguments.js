ac05f15773ea516ef99dea7297380ee5f61b104f
courte_labs <courte_labs@kali.courte_labs>
2022-09-05 16:07:13 +0300

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,7 +5,7 @@ const arg = process.argv;
 if (arg.length === 2){
     console.log("No argument");
 }else if (arg.length === 3){
-    console.log("Argument found")
+    console.log("Argument found");
 }else{
-    console.log("Arguments found")
+    console.log("Arguments found");
 }

a3911d7957948aaf40af2b0685f87bc33f83fb7b
courte_labs <courte_labs@kali.courte_labs>
2022-09-05 20:10:12 +0300

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const arg = parseInt(process.argv[2]);
+let argscount = 0;
+
+if (isNaN(arg)) {
+  console.log('Missing number of occurrences');
+}
+while (argscount < arg) {
+  console.log('C is fun');
+  argscount++;
+}

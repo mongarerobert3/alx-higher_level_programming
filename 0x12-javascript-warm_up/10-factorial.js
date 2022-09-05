004b5471dc4aaf545d5985fe5ed4dfbf794b6830
courte_labs <courte_labs@kali.courte_labs>
2022-09-05 22:07:55 +0300

    Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+function factorial (a) {
+  a = parseInt(process.argv[2]);
+
+  let ans = 1;
+
+  for (let i = 2; i <= a; i++) { ans = ans * i; }
+
+  console.log(ans);
+}
+factorial();

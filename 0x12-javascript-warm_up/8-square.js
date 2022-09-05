733e67f32b6d474fce5a0e70d78ed53137014e45
courte_labs <courte_labs@kali.courte_labs>
2022-09-05 21:41:35 +0300

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const arg = parseInt(process.argv[2]);
+const message = 'X';
+
+if (isNaN(arg)) {
+  console.log('Missing size');
+}
+
+for (let i = 0; i < arg; i++) {
+  console.log(message.repeat(arg));
+}

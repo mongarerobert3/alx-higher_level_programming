a1aa963ea1cc971d6ebc6f164cc764320d998fe2
courte_labs <courte_labs@kali.courte_labs>
2022-09-06 23:29:44 +0300

    Esrever

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return list.sort(() => 1);
+  const revArray = []; // New Reversed Array
+
+  for (let i = list.length - 1; i >= 0; i--) {
+    revArray.push(list[i]);
+  }
+  return revArray;
 };

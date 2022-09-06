4dd1ce9edaf119f5be9f5ed0e13403cc7a5c5f93
courte_labs <courte_labs@kali.courte_labs>
2022-09-06 22:37:04 +0300

    Rectangle #3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  // Dynamic method
+  print () {
+    for (let y = 0; y < this.height; y++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};

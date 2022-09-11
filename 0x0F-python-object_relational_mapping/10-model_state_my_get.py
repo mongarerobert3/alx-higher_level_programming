eb9687dca76133bffff53593d588f246183852ec
courte_labs <courte_labs@kali.courte_labs>
2022-09-11 18:18:08 +0300

    Get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""prints the first State object from the database hbtn_0e_6_usa"""
+
+if __name__ == "__main__":
+
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    first = session.query(State).filter(State.name.like('argv[4]%')
+                                        ).order_by(State.id)
+    for i in first:
+        print("{}: {}".format(i.id, i.name))
+    session.close()

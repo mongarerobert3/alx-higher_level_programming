9600a5d19c9aa6faa2c87183d74e60193d591444
courte_labs <courte_labs@kali.courte_labs>
2022-09-11 12:14:16 +0300

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+
+"""Script to list all stattes from hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        states_name_searched=argv[4]
+        charset="utf8"
+    )
+
+    cur = connection.cursor()
+    cur.execute("SELECT * FROM states WHERE name like BINARY\
+        '%{}%' ORDER BY id ASC;".format(argv[4]))
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+    cur.close()
+    connection.close()

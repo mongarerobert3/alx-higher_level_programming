7c4aed15b80399ab27c05925df79b70614751ca6
courte_labs <courte_labs@kali.courte_labs>
2022-09-11 12:09:01 +0300

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     )
 
     cur = connection.cursor()
-    cur.execute("SELECT * FROM states WHERE name like '%N%'")
+    cur.execute("SELECT * FROM states WHERE name like BINARY 'N%'")
     query_rows = cur.fetchall()
 
     for row in query_rows:

10f7a78683d447f3c26ade1e1a8b135c7deb9843
courte_labs <courte_labs@kali.courte_labs>
2022-09-11 19:00:40 +0300

    Cities in state mandatory

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 '''Definition of the City class'''
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()

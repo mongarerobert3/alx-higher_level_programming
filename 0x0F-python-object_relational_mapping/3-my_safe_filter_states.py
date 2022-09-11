df983ed9e2b8cd1d2c67e0d2bee65380dcd6de50
courte_labs <courte_labs@kali.courte_labs>
2022-09-11 12:40:01 +0300

    SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     )
 
     cur = connection.cursor()
-    cur.execute("SELECT * FROM states WHERE name=%s;"(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name='%s';", (argv[4],))
     query_rows = cur.fetchall()
 
     for row in query_rows:

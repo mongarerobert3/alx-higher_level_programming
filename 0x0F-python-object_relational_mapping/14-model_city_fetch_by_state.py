b1c46d6617874e665d62f838bf637fa1bc39fe72
courte_labs <courte_labs@kali.courte_labs>
2022-09-11 18:59:13 +0300

    Cities in state mandatory

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -2,14 +2,15 @@
 """prints all City objects
 from the database hbtn_0e_14_usa"""
 
-import sys
-from model_state import Base, State
-from model_city import City
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from sqlalchemy.schema import Table
-
 if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from model_city import City
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from sqlalchemy.schema import Table
+
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)

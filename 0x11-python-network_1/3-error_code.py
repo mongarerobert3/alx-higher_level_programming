de783fa254281c9037847da026aa230ba01c4849
mongarerobert3 <mongarerobert3@gmail.com>
2022-09-23 17:17:35 +0300

    POST an email #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+    Python script that takes in a URL,
+    sends a request to the URL and displays the body of the response
+"""
+import urllib.request as request
+import sys
+
+
+if __name__ == "__main__":
+    data = request.parse.urlencode({'email': sys.argv[2]}).encode('ascii')
+    req = request.Request(sys.argv[1], data)
+    with request.urlopen(req) as response:
+        html = response.read()
+        print('{}'.format(html.decode('utf-8')))

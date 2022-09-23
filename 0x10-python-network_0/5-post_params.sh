e328b84cbf40c6e20562908f6e4b41104132d6a3
mongarerobert3 <mongarerobert3@gmail.com>
2022-09-23 11:09:01 +0300

    cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # POST request and displays the body of the response
-curl -sX POST -d "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD" "$1"
+curl "$1" -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"

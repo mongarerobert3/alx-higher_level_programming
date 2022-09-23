1153ed76a13bae7bfb8747847a72d7479bb6ef2e
mongarerobert3 <mongarerobert3@gmail.com>
2022-09-23 17:20:18 +0300

    Error code #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,13 +5,15 @@ request to the passed URL with the email as a parameter, and
 displays the body of the response (decoded in utf-8)
 """
 
-import urllib.request
+import urllib.request as request
 import urllib.parse
 import sys
+from urllib import error
+
 
 if __name__ == '__main__':
-    data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('ascii')
-    req = urllib.request.Request(sys.argv[1], data)
-    with urllib.request.urlopen(req) as r:
-        html = r.read()
-        print('{}'.format(html.decode('utf-8')))
+    try:
+        with request.urlopen(sys.argv[1]) as r:
+            print(r.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))

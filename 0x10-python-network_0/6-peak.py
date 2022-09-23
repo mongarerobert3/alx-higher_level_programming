14bb3edef1f040b643bed38344bedb64eacbd707
mongarerobert3 <mongarerobert3@gmail.com>
2022-09-23 11:24:58 +0300

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,22 +5,20 @@
 def find_peak(list_of_integers):
     """Finds a peak in list_of_integers"""
 
-    if list_of_integers == [] or list_of_integers is None:
+    if list_of_integers is None or list_of_integers == []:
         return None
-
-    def binarySearch(arr, l, r):
-        """Binary search"""
-
-        if l == r:
-            return l
-
-        mid = (r + l) / 2
-        mid = int(mid)
-
-        if arr[mid] > arr[mid + 1]:
-            return binarySearch(arr, l, mid)
-
-        return binarySearch(arr, mid + 1, r)
-
-    return list_of_integers[binarySearch(list_of_integers, 0,
-                                         len(list_of_integers) - 1)]
+    lo = 0
+    hi = len(list_of_integers)
+    mid = ((hi - lo) // 2) + lo
+    mid = int(mid)
+    if hi == 1:
+        return list_of_integers[0]
+    if hi == 2:
+        return max(list_of_integers)
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])

eabc024bc14005cd91860882f9d34520b5cac09b
mongarerobert3 <mongarerobert3@gmail.com>
2022-10-18 16:20:16 +0300

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+//Star wars API
+
+// Including the request module
+const request = require('request');
+const id = (process.argv[2])
+const url = 'https://swapi-api.hbtn.io/api/films/' + id;
+
+request(url, function (err, res, body) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(JSON.parse(body).title);
+});

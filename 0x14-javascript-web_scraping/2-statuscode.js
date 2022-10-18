51a1d244eb0aec2b15d811898d169f97c78aabdf
mongarerobert3 <mongarerobert3@gmail.com>
2022-10-18 16:09:04 +0300

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+// Script that display the status code of a GET request.
+
+// FileSystem
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('code: ', response.statusCode);
+});

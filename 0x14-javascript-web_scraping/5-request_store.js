e1b0746f2c270b9c1eadcca32f6f4edc4e6fb756
mongarerobert3 <mongarerobert3@gmail.com>
2022-10-18 16:46:45 +0300

    Loripsum API

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+// file system
+const fs = require('fs');
+const filename = process.argv[3];
+
+// Including the request module
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, header, body) {
+  if (err) {
+    console.error(err);
+  }
+  fs.writeFile(filename, body, 'utf-8', function (error) {
+    if (error) {
+      console.log(error);
+    }
+  });
+});

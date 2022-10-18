0c453cb10fbdcb9ca2168bd30a20be22a329b6c1
mongarerobert3 <mongarerobert3@gmail.com>
2022-10-18 17:17:36 +0300

    How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,26 @@
 #!/usr/bin/node
+
+// script that computes the number of tasks completed by user id
+// You must use the https://jsonplaceholder.typicode.com/todos API
+
+// Including the axios module
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+
+  const bodyData = JSON.parse(body);
+  const finalResult = {};
+
+  for (const key in bodyData) {
+    if (finalResult[key.userId] && key.completed) {
+      finalResult[key.userId] += 1;
+    } else if (!finalResult[key.userId] && key.completed) {
+      finalResult[key.userId] = 1;
+    }
+  }
+  console.log(finalResult);
+});

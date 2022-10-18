c3ce2876aef0f01fd745dc2424f07667b58351a3
mongarerobert3 <mongarerobert3@gmail.com>
2022-10-18 16:32:38 +0300

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,26 @@
 #!/usr/bin/node
+
+// Star wars API
+
+// Including the request module
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, request, body) {
+  if (err) {
+    console.error(err);
+  }
+  const data = JSON.parse(body).results;
+  let presentIn = 0;
+
+  for (const dataindex in data){
+    const filmCharacters = data[dataindex].characters;
+
+    for (const charindex in filmCharacters){
+        if (filmCharacters[charindex].includes('18')){
+            presentIn++;
+        }
+    }
+  }
+  console.log(presentIn);
+});

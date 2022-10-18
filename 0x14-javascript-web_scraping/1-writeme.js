04d385d0952dba297f4df20b3c3c4d9b090906aa
mongarerobert3 <mongarerobert3@gmail.com>
2022-10-18 16:02:48 +0300

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+// Script that writes to a file
+
+// FileSystem
+const fs = require('fs');
+const file = process.argv[2];
+const string = process.argv[3];
+
+fs.writeFile(file, string, 'utf-8', function (err, content) {
+  if (err) {
+    console.error(err);
+  }
+});
